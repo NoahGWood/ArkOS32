0f06a124b61310ae8f89be0501a12d7103b8ca86
Noah Wood <woodysfoundry@gmail.com>
2017-12-16 16:25:23 -0500

    new file:   .gitignore 	modified:   source/kernel/kernel/kernel.c

--- source/kernel/kernel/kernel.c
+++ source/kernel/kernel/kernel.c
@@ -4,5 +4,7 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+	printf("Welcome to ArkOS!\n");
+    printf("We are currently undergoing some development.\n");
+    printf("Pardon our dust");
 }
