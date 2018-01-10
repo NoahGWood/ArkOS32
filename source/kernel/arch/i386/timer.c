889b7d46817e2f0846d69172ae616196723cc0ea
Noah Wood <woodysfoundry@gmail.com>
2018-01-09 19:57:33 -0500

    modified:   arkos.iso 	new file:   source/kernel/arch/i386/keyboard.c 	modified:   source/kernel/arch/i386/make.config 	modified:   source/kernel/arch/i386/timer.c 	new file:   source/kernel/include/kernel/kb.h 	new file:   source/kernel/include/kernel/multiboot.h 	modified:   source/kernel/kernel/kernel.c

--- source/kernel/arch/i386/timer.c
+++ source/kernel/arch/i386/timer.c
@@ -2,17 +2,29 @@
 
 /* This will track how many ticks the system has been running for */
 int timer_ticks = 0;
+/* This will track the number of seconds the system has been running for */
+int seconds;
+seconds = 0;
+
+void timer_phase(int hz) {
+    int divisor = 1193180 / hz;       /* Calculate divisor */
+    outportb(0x43, 0x36);             /* Set command byte 0x36 */
+    outportb(0x40, divisor & 0xFF);   /* Set low byte of divisor */
+    outportb(0x40, divisor >> 8);     /* Set high byte of divisor */
+}
+		
 
 /* Handles the timer. Increment timer_ticks every
 * time the timer fires. By default, the timer fires 
 * 18.222 times per second. */
 void timer_handler(struct regs *r) {
-    printf("Timer Tick\n");
     /* increment tick count */
     timer_ticks++;
-    /* Ever 18 clocks, display a message */
-    if (timer_ticks % 18 == 0) {
-        printf("One second has passed\n");
+    /* Every second print a message */
+    if (timer_ticks % 100 == 0) {
+        seconds++;
+        char c = seconds;
+//        printf("%c Seconds Since Boot\n", c);
     }
 }
 
@@ -20,4 +32,5 @@ void timer_handler(struct regs *r) {
 void timer_install() {
     printf("Installing Timer\n");
     irq_install_handler(0, timer_handler);
+    timer_phase(100);
 }
--- source/kernel/kernel/kernel.c
+++ source/kernel/kernel/kernel.c
@@ -2,8 +2,9 @@
 
 #include <kernel/tty.h>
 #include <kernel/system.h>
+#include <kernel/multiboot.h>
 
-void kernel_main(void) {
+void kernel_main(multiboot_info_t* mbd, unsigned int magic) {
     terminal_initialize();
     gdt_install();
     idt_install();
@@ -14,6 +15,9 @@ void kernel_main(void) {
 	printf("Welcome to ArkOS!\n");
     printf("We are currently undergoing some development.\n");
     printf("Pardon our dust\n");
+    int i = 102;
+    printf("%s\n", i);
     timer_install();
+    keyboard_install();
     for (;;);
 }
